23cbc3cc1094da3f0b3972eca3ffa3896b0af582
pavel <pavelsysuev20@gmail.com>
2025-09-18 11:42:19 +0400

    Лаба исправлена ещё раз

--- lab_1/test/tests01.cpp
+++ lab_1/test/tests01.cpp
@@ -16,6 +16,12 @@ TEST(test_03, basic_test_set)
     ASSERT_TRUE(plant(150,0,500)==4);
 }
 
+TEST(test_04, basic_test_set)
+{
+    ASSERT_TRUE(plant(5,1,35)==9);
+}
+
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
